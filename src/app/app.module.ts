bf42dc09b02e63e3434af969db724f8cfd2ee6f4
LapMarcos <marcoslaporte2015@gmail.com>
2023-10-30 15:33:30 -0300

    Firebase, bootstrap & favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"la-clinica-111655","appId":"1:139443659844:web:56ea20cc0268b7afb54811","storageBucket":"la-clinica-111655.appspot.com","apiKey":"AIzaSyDhjoZlOyHYyEZqXKF8Txb2QdcBAx6eCJQ","authDomain":"la-clinica-111655.firebaseapp.com","messagingSenderId":"139443659844"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
