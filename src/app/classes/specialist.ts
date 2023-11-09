48a386dbd89542cf04a28abf8097ed9b519d6a87
LapMarcos <marcoslaporte2015@gmail.com>
2023-11-09 00:24:39 -0300

    Specialist shift hours

--- src/app/classes/specialist.ts
+++ src/app/classes/specialist.ts
@@ -5,11 +5,15 @@ export class Specialist extends User {
 	specialty: StringIdValuePair;
 	isEnabled: boolean;
 	workingDays: Array<number>;
+	shiftStart: string = '08:30';
+	shiftEnd: string = '18:30';
 
-	constructor(id: string = '', firstName: string, lastName: string, age: number, idNo: number, imgUrl: string, email: string, password: string, specialty: StringIdValuePair, isEnabled: boolean, workingDays: Array<number>) {
+	constructor(id: string = '', firstName: string, lastName: string, age: number, idNo: number, imgUrl: string, email: string, password: string, specialty: StringIdValuePair, isEnabled: boolean, workingDays: Array<number>, shiftStart: string = '08:30', shiftEnd: string = '18:30') {
 		super('specialist', id, firstName, lastName, age, idNo, imgUrl, '', email, password);
 		this.specialty = specialty;
 		this.isEnabled = isEnabled;
 		this.workingDays = workingDays;
+		this.shiftStart = shiftStart;
+		this.shiftEnd = shiftEnd;
 	}
 }
--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Specialist } from 'src/app/classes/specialist';
 import { User } from 'src/app/classes/user';
 import { ToastSuccess, ToastError, ToastWarning } from 'src/app/environments/environment';
 import { AuthService } from 'src/app/services/auth.service';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
 	selector: 'app-account',
@@ -11,19 +13,25 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class AccountComponent {
 	user: User | null = null;
+	specialist: Specialist | null = null;
 
-	constructor(private auth: AuthService, private router: Router) {
+	constructor(private auth: AuthService, private db: DatabaseService, private router: Router) {
 		this.user = auth.LoggedUser;
+		if (this.user?.role === 'specialist')
+			this.specialist = this.user as Specialist;
 	}
 
-	/* async ngOnInit() {
-		Loader.fire();
-		await this.auth.getLoggedUser()
-			.then((user) => {
-				this.user = user;
-			});
-		Loader.close();
-	} */
+	saveShift() {
+		if (this.specialist!.shiftStart < '08:30' || this.specialist!.shiftEnd > '18:30')
+			ToastError.fire('Oops...', 'Work hours are between 08:30 and 18:30.');
+		else if (this.specialist!.shiftStart >= this.specialist!.shiftEnd)
+			ToastError.fire('Oops...', 'Work hours should not overlap.');
+		else {
+			this.db.updateDoc('users', this.specialist?.id!, this.specialist)
+				.then(() => ToastSuccess.fire('Shift hours updated!'))
+				.catch((error: any) => ToastError.fire('Oops...', error.message));
+		}
+	}
 
 	signOut() {
 		this.auth.signOut()
